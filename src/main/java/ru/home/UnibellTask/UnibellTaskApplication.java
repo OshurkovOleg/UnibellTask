8e17925076dc13e9ab32fc0ec73c48ce5f40ecb8
Oshurkov <oshurkov.oc@gmail.com>
2023-09-01 13:50:45 +0500

    start project

---
+++ src/main/java/ru/home/UnibellTask/UnibellTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.home.UnibellTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibellTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnibellTaskApplication.class, args);
+	}
+
+}
