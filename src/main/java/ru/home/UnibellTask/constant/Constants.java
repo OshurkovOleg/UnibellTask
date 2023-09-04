535c75d3d954b86a919e3ae3d3ad604e25ab2d30
Oshurkov <oshurkov.oc@gmail.com>
2023-09-04 22:11:15 +0500

    change lang logs

--- src/main/java/ru/home/UnibellTask/constant/Constants.java
+++ src/main/java/ru/home/UnibellTask/constant/Constants.java
@@ -6,12 +6,12 @@ public class Constants {
     public static final String PHONE_NOT_FOUND = "Phone not found";
     public static final String EMAIL = "email";
     public static final String PHONE = "phone";
-    public static final String CLIENT_ADD_SUCCESSFULLY = "Клиент добавлен успешно: ";
-    public static final String PHONE_NUMBER_ADD_SUCCESSFULLY = "Номер телефона добавлен успешно: ";
-    public static final String MAIL_ADDRESS_ADD_SUCCESSFULLY = "Адрес почты добавлен успешно: ";
-    public static final String REQUEST_CLIENT_ID = "Запрос клиента по id";
-    public static final String REQUEST_ALL_CUSTOMERS = "Запрос всех клиентов";
-    public static final String REQUEST_ALL_CONTACT_DATA_FOR_CLIENT = "Запрос всех контактных данных по клиенту";
-    public static final String REQUEST_CONTACT_DATA_CLIENT_ID_SPECIFIED_CONTACT_TYPE = "Запрос контакных данных по id клиента, по указанному типу контакта";
+    public static final String CLIENT_ADD_SUCCESSFULLY = "Client added successfully: ";
+    public static final String PHONE_NUMBER_ADD_SUCCESSFULLY = "Phone number added successfully: ";
+    public static final String MAIL_ADDRESS_ADD_SUCCESSFULLY = "Email added successfully: ";
+    public static final String REQUEST_CLIENT_ID = "Client request by id";
+    public static final String REQUEST_ALL_CUSTOMERS = "Request all clients";
+    public static final String REQUEST_ALL_CONTACT_DATA_FOR_CLIENT = "Request all contact information for a client";
+    public static final String REQUEST_CONTACT_DATA_CLIENT_ID_SPECIFIED_CONTACT_TYPE = "Request for contact data by client id, by the specified contact type";
 
 }
